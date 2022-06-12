7730a40dae14426df7d1d3fdc0e8dd742be9ed7d
melianasftr <melianasftr2@gmail.com>
2022-06-12 18:55:51 +0700

    add.login

--- Frontend/src/components/login/login.jsx
+++ Frontend/src/components/login/login.jsx
@@ -1,29 +1,24 @@
-import './App.css';
-import profile from "./image/login.jpg";
+import profile from "../../Profile-login.png"
+import "./login.css"
 
 function App() {
   return (
     <div className="main">
-      <div className="sub-main">
-        <div>
-          <div className="imgs">
-            <div className="container-image">
-              <img src={profile} alt= "profile" className="profile"/>
-
-              </div>
-
-              </div>
-              </div>
-                <h1>MASUK</h1>
-            <div>
-              <input type="text" placeholder="your email"className="name"/>
-          </div>
-              <div className="second.inpput">
-              <input type="text" placeholder="your password"className="name"/>
-        </div>
-          
+    <h1 className="text-masuk"> Masuk </h1>
+      <div className="images"> 
+          <img src={profile} alt="" />
       </div>
-    </div>  
+      <form>
+        <div className="email">
+        <input className="text-email" type="email" placeholder="your email" />
+        </div> 
+     <div className="password">
+     <input className="text-password" type="password" placeholder="your password" />
+      </div>
+      <button>Masuk</button>
+      <p className="text-white">dont have an account? <a href="signup"> sign up </a> </p>
+      </form>
+    </div>   
   );
 }
 
