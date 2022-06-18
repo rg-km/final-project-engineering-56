276227cad3261575f354b53be2b3f46d461eabae
Nanda <Nandasulaiman43@gmail.com>
2022-06-18 15:59:20 +0700

    add hash password

---
+++ Backend/middleware/hash.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Hashpassword(password string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return string(hash)
+}
+
+func Verifypassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
