45ccfd49965477c6ffa9505b972bdf536ccb72e7
sayfiqireal <ahmadmuflihtaufiqi@gmail.com>
2022-06-20 19:10:58 +0700

    admin

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Admin from "./pages/admin";
+import Tambah from "./pages/tambah";
+import Edit from "./pages/edit";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main aria-label="App" className="App">
+      <div className="routes" aria-label="routes">
+        <Routes>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/tambah" element={<Tambah />} />
+          <Route path="/edit" element={<Edit />} />
+        </Routes>
+      </div>
+    </main>
   );
 }
 
